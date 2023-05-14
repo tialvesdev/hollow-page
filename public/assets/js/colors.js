b8fafa18e882a1fce1c64936a4a0724a6b6d0262
Tiago Alves <tchaguwu@gmail.com>
2023-05-14 20:57:30 -0300

    Feed Style

--- public/assets/js/colors.js
+++ public/assets/js/colors.js
@@ -11,6 +11,7 @@ var colors = [
 var selectedColor = '';
 
 function trocaCorTema(color) {
+
     var elements = document.querySelectorAll(
         '.grimm1, .grimm2, .crystal, .dreams, .lifeblood, .godmaster'
     );
@@ -18,9 +19,22 @@ function trocaCorTema(color) {
     if (color == 0) {
         elements.forEach(element => element.style.removeProperty('color'));
 
+        document.querySelectorAll('.dream-efx-img').forEach((element) => {
+            var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+            element.style.filter = `invert(97%) sepia(97%) saturate(4%) hue-rotate(77deg) brightness(104%) contrast(100%) 
+            drop-shadow(0 0 5px ${randomColor}) drop-shadow(0 0 10px ${randomColor}) drop-shadow(0 0 15px ${randomColor}) blur(${Math.floor(Math.random() * 10)}px)`;
+        });
+
         selectedColor = '';
     } else {
         elements.forEach(element => element.style.color = colors[color]);
+
+        document.querySelectorAll('.dream-efx-img').forEach((element) => {
+            
+            element.style.filter = `invert(97%) sepia(97%) saturate(4%) hue-rotate(77deg) brightness(104%) contrast(100%) 
+            drop-shadow(0 0 5px ${colors[color]}) drop-shadow(0 0 10px ${colors[color]}) drop-shadow(0 0 15px ${colors[color]}) blur(${Math.floor(Math.random() * 10)}px)`;
+        });
 
         selectedColor = colors[color];
     }
